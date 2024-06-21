a412771a779e18f74fb4f88f7b69f0618375f58f
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-06-21 17:56:36 +0300

    тесты фреймворка

--- app_library.py
+++ app_library.py
@@ -1,1 +1,28 @@
 from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world'
+
+@app.route('/about') # маршрут
+def about(): # функция действия в коце маршрута
+    return f'This is about page' #то что будет на странице, когда перейдем по маршруту
+
+@app.route('/user/<username>')
+
+def show_user_profile(username):
+    return f' User {username}'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', name=name)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
