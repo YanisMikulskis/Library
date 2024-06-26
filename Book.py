bb0cf5cf0d23d46760f0c8fc882af8864473d07d
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-06-26 15:14:11 +0300

    Прописывание методов библиотеки

--- Book.py
+++ Book.py
@@ -1,8 +1,69 @@
+# from selenium import webdriver
+#
+#
+#
+# driver = webdriver.Safari()
+# driver.get('https://readly.ru/books/i_am_lucky/?show=1')
+# element = driver.find_element_by_css_selector('<button class="btn-orange-big-20">Мне повезет</button>')
+# element.click()
+
+
+from random import randint
+import webbrowser
+from bs4 import BeautifulSoup
+import requests
+import re
+# create webdriver object
+# driver = webdriver.Safari()
+
+# get lambdatest
+# driver.get("https://readly.ru/books/i_am_lucky/?show=1")
+# for i in range(3):
+#     url = "https://readly.ru/books/i_am_lucky/?show=1"
+#     webbrowser.open_new(url)
+# url = "https://readly.ru/books/i_am_lucky/?show=1"
+# for i in range(3):
+#     request_html = requests.get(url)
+#     soup = BeautifulSoup(request_html.text, 'html.parser')
+#
+#     name_book = re.findall(r'>(.+)</a>', str(soup.find_all(class_='blvi__title')))
+#     author_book = re.search(r'>([а-яА-Я ]+)<', str(soup.find_all(class_='blvi__book_info')))
+#     year_book = [i for i in book_info.split() if len(i) == 4 and i.isdigit()]
 
 class Book:
-    def __init__(self, title, author, year, is_checked_out, personal_number):
-        self.title = title
-        self.author = author
-        self.year = year
-        self.is_checked_out = is_checked_out
-        self.personal_number = personal_number
+    def __init__(self):
+        self.title = None
+        self.author = None
+        self.year = None
+        self.is_checked_out = None
+        self.personal_number = None
+    def generic_data(self):
+        url = "https://readly.ru/books/i_am_lucky/?show=1"
+        request_html = requests.get(url)
+        soup = BeautifulSoup(request_html.text, 'html.parser')
+        self.title = re.findall(r'>(.+)</a>', str(soup.find_all(class_='blvi__title')))[0]
+        book_info =  str(soup.find_all(class_='blvi__book_info'))
+        self.author = re.findall(r'>([а-яА-Я ]+)<', book_info)[0]
+        try: #если поле года каким то образом оказалось пустым
+            self.year = [i for i in book_info.split() if len(i) == 4 and i.isdigit()][0]
+        except IndexError:
+            self.year = 0000
+        # self.personal_number = randint(10000, 99999)
+        # self.all_data = [self.title, self.author, self.year, self.is_checked_out, self.personal_number]
+        self.book_data = [self.title, self.author, self.year]
+    def __str__(self):
+        return (f'{self.title}\n'
+                f'{self.author}\n'
+                f'{self.year}\n'
+                f'{self.is_checked_out}\n'
+                f'{self.personal_number}')
+
+def make_books(quantity):
+    all_book_dates = []
+    book = Book()
+    for i in range(quantity):
+        book.generic_data()
+        print(book.book_data)
+        all_book_dates.append(book.book_data)
+    print(f'all book datessss {all_book_dates}')
+    return all_book_dates
--- Library.py
+++ Library.py
@@ -1,12 +1,15 @@
-from Book import Book
+from Book import make_books
 from User import User
 import sqlite3
 import random
+
+
 class library_DB:
     def __init__(self, name_database):
         self.name_database = name_database
         self.connection = sqlite3.connect(self.name_database)
         self.cursor = self.connection.cursor()
+
     def create_tables(self):
         self.cursor.execute(
             '''
@@ -28,12 +31,13 @@ class library_DB:
             author VARCHAR(100) NOT NULL,
             year VARCHAR(4) NOT NULL, 
             is_checked_out BLOB NOT NULL,
-            personal_number INTEGER NOT NULL UNIQUE,
+            personal_number INTEGER NOT NULL,
             user_book_id INTEGER,
             FOREIGN KEY(user_book_id) REFERENCES Users_db(id)
             )
             '''
         )
+
     def drop(self):
         self.cursor.execute('''DROP TABLE IF EXISTS Library_db;''')
         # self.cursor.execute('''DROP TABLE IF EXISTS Users_db;''')
@@ -51,18 +55,18 @@ class library_DB:
         is_checked_out_var = 5
         personal_number_var = 353832467
 
-        db_library.cursor.execute('''PRAGMA foreign_keys = ON;''') # проверка существования id пользователя !!!
+        db_library.cursor.execute('''PRAGMA foreign_keys = ON;''')  # проверка существования id пользователя !!!
         db_library.cursor.execute('''INSERT INTO Library_db
                                      (title, author, year, is_checked_out, personal_number, user_book_id)
                                      VALUES('sdsgsd', 'fdsgsdg', '3223', 5, 352, null);''')
-
 
         self.connection.commit()
 
     def foreight(self):
         self.cursor.execute('''UPDATE Library_db SET user_book_id=:user_book_id WHERE id=:id;''',
-                               dict(user_book_id=5, id=1))
+                            dict(user_book_id=5, id=1))
         self.connection.commit()
+
     def select(self):
         self.cursor.execute('''SELECT Library_db.title, Library_db.user_book_id, Users_db.name
                                 FROM Library_db
@@ -85,6 +89,8 @@ class library_DB:
         self.cursor.execute('''UPDATE Library_db SET user_book_id = (SELECT id FROM Users_db WHERE name='ivan') 
                                 WHERE personal_number = 3532;''')
         self.connection.commit()
+
+
 db_library = library_DB('Library_Database')
 db_library.create_tables()
 
@@ -107,43 +113,99 @@ db_library.create_tables()
 class Library:
     def __init__(self):
         self.DB = db_library
-    def add_book(self, *args):
-        title, author, year, is_checked_out, personal_number = args
-        self.DB.cursor.execute('''INSERT INTO Library_db
-                                (title, author, year, is_checked_out, personal_number, user_book_id)
-                                VALUES(?, ?, ?, ?, ?, null);''', [title,
-                                                                            author,
-                                                                            year,
-                                                                            is_checked_out,
-                                                                            personal_number])
 
-        self.DB.connection.commit()
-    def remove_book(self, id_book):
-        self.DB.cursor.execute('''DELETE FROM Library_db WHERE id=:id;''', dict(id=id_book))
+    @staticmethod
+    def number_extraction():
+        db_library.cursor.execute('''SELECT personal_number FROM Library_db;''')
+        all_personal_numbers = list(map(lambda number_tuple: number_tuple[0], db_library.cursor.fetchall()))
+        return all_personal_numbers
+
+    def add_book(self):
+        def query_insert(data):
+            self.DB.cursor.execute('''INSERT INTO Library_db
+                                                    (title, author, year, is_checked_out, personal_number, user_book_id)
+                                                    VALUES(?, ?, ?, ?, ?, null);''', data)
+            self.DB.connection.commit()
+
+        choice = int(input('Добавить случайную книгу из сети - нажмите 1\n'
+                           'Ввести вручную данные - нажмите 2\n'
+                           'Вернуться в главное меню программы - нажмите 3\n'))
+        if choice == 1:
+            quantity = int(input('Сколько случайных книг нужно добавить?'))
+            all_book_data = make_books(quantity=quantity)
+            for insert in range(quantity):
+                all_data = [*all_book_data[insert], 0, self.number_extraction()[-1] + 1]
+                query_insert(data=all_data)
+
+        elif choice == 2:
+            title = input('Введите название книги')
+            author = input('Введите автора книги')
+            year = input('Введите год издания')
+            personal_number = self.number_extraction()[-1] + 1
+            all_data = [title, author, year, 0, personal_number]
+            query_insert(data=all_data)
+        elif choice == 3:
+            main()
+
+        else:
+            print(f'Ошибка! введите 1 или 2')
+            self.add_book()
+
+    def remove_book(self):
+        id_remove_book = int(input(f'Какую книгу нужно удалить? введите ID'))
+        self.DB.cursor.execute('''DELETE FROM Library_db WHERE id=:id;''', dict(id=id_remove_book))
         self.DB.cursor.execute('''UPDATE sqlite_sequence set seq = 0 WHERE name = 'Library_db';''')
         self.DB.connection.commit()
+
     def find_book(self):
-        self.DB.cursor.execute('''SELECT * FROM Library_db WHERE title=:title;''', dict(title='Война и мир'))
-        print(self.DB.cursor.fetchall())
+        title = input(f'Введите название книги, которую нужно найти')
+        self.DB.cursor.execute('''SELECT * FROM Library_db WHERE title=:title;''', dict(title=title))
+        find_books = self.DB.cursor.fetchall()
+        print(find_books)
+        for book in find_books:
+            availability = f'Есть' if not book[4] else f'На руках'
+            print(f'id: {book[0]}\n'
+                  f'Название: {book[1]}\n'
+                  f'Автор: {book[2]}\n'
+                  f'Год издания: {book[3]}\n'
+                  f'Наличие: {availability}\n'
+                  f'Индивидуальный номер: {book[5]}\n'
+                  f'У кого книга {book[6]}')
+            if book[6]:
+                self.DB.cursor.execute('''SELECT Users_db.name
+                                       FROM Library_db
+                                       JOIN Users_db ON Users_db.id = Library_db.user_book_id;''')
+                print(self.DB.cursor.fetchall())
+
+
+
     def register_user(self, *user_data):
         name, email = user_data
         self.DB.cursor.execute('''INSERT INTO Users_db(name, email) VALUES(?, ?);''', (name, email))
         self.DB.connection.commit()
 
-    def checkout_book(self, user_id):
-
+    def checkout_book(self):
         user_id = user_id
         personal_number = 32532
         self.DB.cursor.execute('''UPDATE Library_db SET user_book_id = (SELECT id FROM Users_db WHERE id=:id)
-                                    WHERE personal_number=:personal_number ;''', dict(id=user_id, personal_number=personal_number))
+                                    WHERE personal_number=:personal_number ;''',
+                               dict(id=user_id, personal_number=personal_number))
         self.DB.connection.commit()
 
         # self.cursor.execute('''UPDATE Library_db SET user_book_id = (SELECT id FROM Users_db WHERE name='ivan')
         #                                 WHERE personal_number = 3532;''')
+
     def return_book(self):
         self.DB.cursor.execute('''UPDATE Library_db SET user_book_id = null WHERE user_book_id = 4;''')
         self.DB.connection.commit()
-    def generate_report(self):
+
+    def checkout_report(self):
+        print(f'PASS')
+
+    def return_report(self):
+        ...
+
+    def general_report(self):
         name = 'VASYA'
         # self.DB.cursor.execute('''SELECT Library_db.title, Library_db.user_book_id, Users_db.name
         #                         FROM Library_db
@@ -158,7 +220,6 @@ class Library:
         all_books = self.DB.cursor.fetchall()
         book_in_library = [book for book in all_books if book[6] is None]
 
-
         self.DB.cursor.execute('''SELECT Library_db.id, Library_db.title, Library_db.user_book_id, Users_db.name
                                     FROM Library_db
                                     JOIN Users_db ON Users_db.id=Library_db.user_book_id;''')
@@ -166,52 +227,40 @@ class Library:
 
 
 lib = Library()
+
 print(f'Добро пожаловать в онлайн библиотеку!')
 commands = {
-    1:
-    2:
-    3:
-    4:
-    5:
+    1: lambda: lib.add_book(),
+    2: lambda: lib.remove_book(),
+    3: lambda: lib.find_book(),
+    4: lambda: lib.register_user(),
+    5: lambda: lib.checkout_book(),
+    6: lambda: lib.return_book(),
+    7: lambda: lib.checkout_report(),
+    8: lambda: lib.return_report(),
+    9: lambda: lib.general_report()
 }
-while 1:
-    # Добавление, удаление
-    # и
-    # поиск
-    # книг
-    # в
-    # библиотеке.
 
-    # Регистрация
-    # пользователей
-    # библиотеки.
 
-    # Взятие
-    # и
-    # возврат
-    # книг
-    # пользователями.
+def main():
+    while 1:
+        start_app = int(input('Что хотите сделать? Введите соответсвующую цифру!\n'
+                              '1 - Добавить книгу в библиотеку\n'
+                              '2 - Удалить книгу из библиотеки по ID\n'
+                              '3 - Найти книгу в библиотеке по ID\n'
+                              '4 - Зарегистрировать нового читателя\n'
+                              '5 - Выдать читателю книгу\n'
+                              '6 - Оформить возврата книги читателем\n'
+                              '7 - Составить отчет о книгах на руках\n'
+                              '8 - Составить отчет о книгах в библиотеке\n'
+                              '9 - Составить общий отчет о состоянии библиотеки\n'))
 
-    # Ведение
-    # учета
-    # выдачи
-    # книг
-    # и
-    # возвратов.
+        commands[start_app]()
 
-    # Генерация
-    # отчетов
-    # о
-    # текущем
-    # состоянии
-    # библиотеки.
 
-    start_app = int(input('Что хотите сделать? Введите соответсвующую цифру!\n'
-                          '1 - Добавить книгу в библиотеку'
-                          '2 - Удалить книгу из библиотеки'
-                          ''))
+main()
 # lib.add_book('Капитанская дочка', 'А.С.Пушкин', 1836, 0, random.randint(1000,9999))
 # lib.remove_book(3)
-#lib.register_book('VASYA', 'VAS@ail.ru')
+# lib.register_book('VASYA', 'VAS@ail.ru')
 # lib.checkout_book(1)
 # lib.return_book()
