af576b2424f3c931d556d54f6ebcd128a392e978
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-07-04 12:54:50 +0300

    Начинаю делать на Flask

--- Flask/app_library.py
+++ Flask/app_library.py
@@ -1,30 +1,86 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
+from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    return 'Hello, world'
+def hello_library():
+    return f'Hello, Library!'
 
-@app.route('/about') # маршрут
-def about(): # функция действия в коце маршрута
-    return f'This is about page' #то что будет на странице, когда перейдем по маршруту
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f' User {username}'
 
-@app.route('/hello/<name>')
-def hello(name):
-    return render_template('hello.html', name=name)
 
-@app.route('/greet/<username>')
-def greet(username):
-    return f'Hello, dear {username}!!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, world'
+#
+# @app.route('/about') # маршрут
+# def about(): # функция действия в конце маршрута
+#     return f'This is about page' #то что будет на странице, когда перейдем по маршруту
+#
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     return f' User {username}'
+#
+# @app.route('/hello/<name>')
+# def hello(name):
+#     surname = 'смирнов'
+#     return render_template('hello.html', name=name, surname=surname)
+#
+# @app.route('/greet/<username>')
+# def greet(username):
+#     return f'Hello, dear {username}!!'
+#
+#
+#
+# @app.route('/submit', methods = ['POST', 'GET'])
+# def submit():
+#
+#     if request.method == 'POST': #если нажимаем кнопку submit (тут POST означает именно ОТПРАВКУ данных)
+#
+#         name = request.form.get('name') #забираем имя которое мы ввели ранее в форме
+#         surname = request.form.get('surname')
+#         return f'hello, forms and {len(name)}  {surname}!!'
+#
+#     return render_template('reg.html') #при переходе по маршруту сразу генерится форма
+#
+#     # return render_template('reg.html')
+#
+#
+# @app.route('/data')
+# def data():
+#     return jsonify({'key':'value'})
+#
+#
+#
+#
+#
+# #подключение к БД
+#
+#
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+# db = SQLAlchemy(app)
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True,nullable=False)
+#     email = db.Column(db.String(120), unique=True,nullable=False)
+#
+#     def __repr__(self):
+#         return f'<User {self.username}>'
+#
+#
+# new_user = User(username='newuser',email='newuser@mail.ru')
+# db.session.add(new_user)
+# db.session.commit()
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 
