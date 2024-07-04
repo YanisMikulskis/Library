21d5eab8b9dce41f3f9a756949f36f789bf1f63e
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-07-04 21:14:02 +0300

    Создал таблицы данных для Flask

---
+++ Flask/createDB.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+
+connection = sqlite3.connect('Library_FLASK_DB')
+cursor = connection.cursor()
+
+
+
+cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS Users_db
+            (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE
+            )
+            '''
+        )
+cursor.execute(''' 
+            CREATE TABLE IF NOT EXISTS Library_table_flask
+            (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title VARCHAR(150) NOT NULL,
+            author VARCHAR(100) NOT NULL,
+            year VARCHAR(4) NOT NULL,
+            is_checked_out BLOB NOT NULL,
+            personal_number INTEGER NOT NULL UNIQUE,
+            user_book_id INTEGER,
+            FOREIGN KEY(user_book_id) REFERENCES User_table_flask(id)
+            )
+            '''
+               )
+
+
