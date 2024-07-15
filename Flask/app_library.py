97bbbb64738289c6b7a7724ff5e9fb0794843de2
Yanis Mikulskis <www.titan.ru95@mail.ru>
2024-07-15 23:28:34 +0300

    Делаю шаблоны и обработку запросов

--- Flask/app_library.py
+++ Flask/app_library.py
@@ -1,24 +1,108 @@
+import functools
 import sqlite3
 
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask import request
 from flask import jsonify
+from flask import flash
 
 # from sqlalchemy import create_engine, Column, Integer, String,Boolean, select, MetaData, ForeignKey
 # from sqlalchemy.inspection import inspect
 # from sqlalchemy.orm import DeclarativeBase, sessionmaker, relationship
 
 from createDB import *
+
 app = Flask(__name__)
+app.secret_key = 'my_library'
+
+
 @app.route('/')
 def book():
+    print('запущено')
     return f'{user_have_books}'
+
 
 @app.route('/home')
 def home():
     return render_template('hello.html')
-#
+
+
+@app.errorhandler(404)
+def error404(error):
+    return render_template('error404.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username:
+            return redirect(url_for('home'))
+        else:
+            return error404(404)
+    return render_template('login.html')
+
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+
+@app.route('/add_book', methods=['POST', 'GET'])
+def add_book():
+    if request.method == 'POST':
+        title_book = request.form['title_book']
+        author_book = request.form['author_book']
+        year_book = request.form['year_book']
+        number_book = (db_session.query(Library_Flask)
+                       .order_by(Library_Flask.personal_number.desc())
+                       .first().personal_number) + 1
+        new_book = Library_Flask(title=title_book,
+                                 author=author_book,
+                                 year=year_book,
+                                 is_checked_out=1,
+                                 personal_number=number_book,
+                                 user_book_id=None)
+        flash(message='Книга добавлена!')
+        data_book = [title_book, author_book, year_book]
+        if all(data_book):
+            db_session.add_all([new_book])
+            db_session.commit()
+    else:
+        flash(message='Введите данные')
+    return render_template('add_book_ordinary.html')
+
+@app.route('/remove_book', methods = ['POST', 'GET'])
+def remove_book():
+
+
+@app.route('/books_in_library')
+def books_in_library():
+    all_books = db_session.query(Library_Flask).filter(Library_Flask.user_book_id == None).all()
+    books = [book for book in all_books]
+    books_dict = {
+        'Общая база книг в библиотеке': []
+    }
+
+    for book in books:
+        dict_book = {
+            'personal_number': book.personal_number,
+            'title': book.title,
+            'author': book.author,
+            'year': book.year,
+            'count': db_session.query(func.count(Library_Flask.id).filter(Library_Flask.title == book.title)).scalar()
+        }
+        books_dict['Общая база книг в библиотеке'].append(dict_book)
+
+    print(books_dict)
+
+    return render_template('books_in_library.html', books_dict=books_dict)
+
+
 # def connect_db():
 #     connection = sqlite3.connect('Library_FLASK_DB')
 #     connection.row_factory = sqlite3.Row
@@ -41,14 +125,13 @@ def home():
 #
 # @app.route('/home')
 # def home():
-#     return render_template('reg.html')
+#     return render_template('menu.html')
 #
 #
 #
 #
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
 
 
 # @app.route('/')
@@ -83,9 +166,9 @@ def home():
 #         surname = request.form.get('surname')
 #         return f'hello, forms and {len(name)}  {surname}!!'
 #
-#     return render_template('reg.html') #при переходе по маршруту сразу генерится форма
+#     return render_template('menu.html') #при переходе по маршруту сразу генерится форма
 #
-#     # return render_template('reg.html')
+#     # return render_template('menu.html')
 #
 #
 # @app.route('/data')
@@ -118,9 +201,3 @@ def home():
 #
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
--- Flask/createDB.py
+++ Flask/createDB.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy import create_engine, Column, Integer, String,Boolean, select, MetaData, ForeignKey
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, relationship
+from sqlalchemy import desc, func
 import os
 engine = create_engine('sqlite:///Library_FLASK_DB')
 metadata = MetaData()
@@ -37,17 +38,62 @@ class User_Flask(Base):
 Base.metadata.create_all(bind=engine)
 session = sessionmaker(autoflush=False, bind=engine)
 db_session = session()
-book_test = db_session.query(Library_Flask).filter(Library_Flask.id==78).first()
+#вывести название книги по id в условии (может варьироваться)
+book_test = db_session.query(Library_Flask).filter(Library_Flask.id==78).first().title
+
 user_test = db_session.query(User_Flask).filter(User_Flask.id==6).first()
 
 user_have_books = db_session.query(User_Flask).filter(User_Flask.name=='Виктория').first().name
-print(user_have_books)
+# print(user_have_books)
+book_in_library = db_session.query(Library_Flask).filter(Library_Flask.user_book_id==0).all()
+
+#Какие книги находятся у Виктории?
+book_vik = db_session.query(User_Flask).filter(User_Flask.name=='Виктория').first().books
+#Выдадим Виктории книгу Стивена Кинга
+book_vik_add = db_session.query(User_Flask).filter(User_Flask.name=='Виктория').first() #Виктория
+King = db_session.query(Library_Flask).filter(Library_Flask.author=='Стивен Кинг').first() #Кинг
+book_vik_add.books.extend([King]) if King.user_book_id is None else ...#Выдаем
+db_session.commit() #Сохраняем изменения в БД
+
+
+#Какие книги в библиотеке?
+all_books = db_session.query(Library_Flask).filter(Library_Flask.user_book_id==None).all()
+
+
+#Каких в библиотеке нет?
+
+# not_books = [book.user_book_id for book in db_session.query(Library_Flask).all()]
+not_books = db_session.query(Library_Flask).filter(Library_Flask.user_book_id==None).all()
+# print(f'В библиотеке сейчас следущие книги:\n')
+# for book in not_books:
+#     print(book.title)
+#Какие читатели взяли хотя бы одну книгу?
 
 
 
-book_test.users=user_test
-db_session.commit()
+#добавить кингу в библиотеку
+last_number = db_session.query(Library_Flask).order_by(Library_Flask.id.desc()).all()[1].personal_number
 
+# выше обратная сортировка таблицы(order_by(Library_Flask.id.desc())), вывод списка всех результатов, дальше индекс а потом атрибут
+
+test_book = Library_Flask(title='title test',
+                          author='title author',
+                          year='title year',
+                          is_checked_out=1,
+                          personal_number = last_number + 1,
+                          user_book_id = None) # создаем книгу
+# db_session.add_all([test_book]) # добавляем в бд
+# db_session.commit()# коммитим
+counts = {
+
+}
+#пожсчет элементов в таблицу по элементу столбца "в данном случае по названию книги"
+lines_title = db_session.query(func.count(Library_Flask.id).filter(Library_Flask.title == 'Джон Картер на Марсе')).scalar()
+
+#порядковый номер последнего элемента
+last_number = db_session.query(Library_Flask).order_by(Library_Flask.personal_number.desc()).first().personal_number
+
+print(f'last_number {last_number}')
 
 
 # books = db_session.query(Library_Flask).all()
